02b0ca9c74917d53abc7d1657f63cc3adde8c2a7
Artem Moskalenko <artem.moskalenko@decartel.co>
2024-07-08 18:36:28 +0400

    Mapping translation data onto the armature. Though the initial translation parcing is incorrect

--- transfer_keyframes.py
+++ transfer_keyframes.py
@@ -3,24 +3,30 @@ import os
 
 scene = bpy.context.scene
 list_of_empties = []
+list_of_bones = []
 
 #Getting the lenght on the animation sequence
+scene.frame_start = 0
+start_sequence_frame = 0
 last_marker = max(scene.timeline_markers, key=lambda marker: marker.frame)
 scene.frame_end = last_marker.frame
+#keyframe_range = len(range(start_sequence_frame, last_marker.frame + 1))
+keyframe_range = range(start_sequence_frame, 4)
 
 #Selecting the target armature
 def find_armature():
+    print("Looking for the Armature")
     bpy.ops.object.select_all(action='DESELECT')
     for obj in bpy.context.scene.objects:
         if obj.type == 'ARMATURE':
-            obj.select_set(True)
-            bpy.context.view_layer.objects.active = obj
+            bpy.context.scene.objects.active = obj
+            obj.select = True
+            return obj
     #TODO Handle exceptions
 
 #Selecting the reference Empty
 def find_empty():
     print("Looking for Root Empty")
-
     bpy.ops.object.select_all(action='DESELECT')
     for obj in bpy.context.scene.objects:
         if obj.type == 'EMPTY' and obj.name == "Bip01":
@@ -31,52 +37,98 @@ def find_empty():
 
 #Getting Transform data for one of the empties
 def get_empty_transforms(empty):
-     empty_transforms = {}
-     for current_frame in range(last_marker.frame + 1):
+     single_empty_transforms = {}
+     for current_frame in keyframe_range:
          bpy.context.scene.frame_set(current_frame)
          print("Getting transform data for {0}, frame #{1}".format(empty.name, current_frame))
-         empty_transforms[current_frame] = {
+         single_empty_transforms[current_frame] = {
               'location' : empty.location,
               'rotation' : empty.rotation_quaternion,
               'scale' : empty.scale
          }
-     return empty_transforms
+     if empty.name == "Bip01":
+        print("Transforms:", single_empty_transforms)
+     return single_empty_transforms
          
+#Storing Empties' biases
+def get_transform_bias(transforms):
+    transform_bias = {}
+    for current_empty_name in list_of_empties:
+        for transform in transforms:
+            if current_empty_name == transform:
+                transform_bias[current_empty_name] = {
+                    current_empty_name : transforms[transform][0]
+                }
+    return transform_bias
+
 #Getting all of the Empty's Children names
 def fetch_empty_names(empty):
     for child in empty.children:
         list_of_empties.append(child.name)
-        print(child.name)
+        #print(child.name)
         fetch_empty_names(child)
 
+#Collecting all of the transform data per Empty per Frame
 def fetch_empty_keyframes(empty):
-    empty_transforms = {}
-    index = 0
+    all_empty_transforms = {}
     list_of_empties.append(empty.name)  #Adding the first (parent) empty
     fetch_empty_names(empty)            #Adding all of the child empties
-    print(list_of_empties)              #Debug
+    print("List of Empties: ", list_of_empties)              #Debug
 
     for current_empty_name in list_of_empties:
          for obj in bpy.context.scene.objects:
               if obj.name == current_empty_name:
                 bpy.context.scene.objects.active = obj
                 obj.select = True
-                empty_transforms[index] = {current_empty_name : get_empty_transforms(obj)}
-                index += 1
-    return empty_transforms
+                #empty_transforms[index] = {current_empty_name : get_empty_transforms(obj)}
+                all_empty_transforms[current_empty_name] = get_empty_transforms(obj)
+    return all_empty_transforms
          
+#Setting all of the Transform data per Bone per frame
+def set_armature_keyframes(armature, ref_transforms, transform_bias):
+    bpy.ops.object.posemode_toggle()
+    for bone in armature.pose.bones:
+        for ref_name in ref_transforms:
+            if bone.name == ref_name:
+                #print("I have matched {0} with {1}, setting up the keyframes".format(bone.name, ref_name))
+                for current_frame in keyframe_range:
+                    bpy.context.scene.frame_set(current_frame)
+                    #print("Empty location is {0}, Bip's starting location is {1}, and the bias is {2}, so the final location would be {3}".format(ref_transforms[ref_name][current_frame]['location'], bone.location, transform_bias[ref_name]['location'], ref_transforms[ref_name][current_frame]['location'] - transform_bias[ref_name]['location']))
+                    #ref_transforms[ref_name][current_frame]['scale']
+                    bone.location = ref_transforms[ref_name][current_frame]['location']
+                    bone.keyframe_insert(data_path="location", frame=current_frame)
 
-def struc_for_arm(armature):
-    print(armature)
+                    bone.rotation_quaternion = ref_transforms[ref_name][current_frame]['rotation']
+                    bone.keyframe_insert(data_path="rotation_quaternion", frame=current_frame)
+
+                    bone.scale = ref_transforms[ref_name][current_frame]['scale']
+                    bone.keyframe_insert(data_path="scale", frame=current_frame)
+    bpy.ops.object.posemode_toggle()
+
+
+
+    
+
+
+
 
 def transfer_keyframes():
-    empty = find_empty()
-    get_empty_transforms(empty)
-    ref_transforms = fetch_empty_keyframes(empty)
-    find_armature()
-
+    empty = find_empty()                                                #Finding the root Empty
+    ref_transforms = fetch_empty_keyframes(empty)                       #Collecting all of the transform data per Empty per Frame
+    transform_bias = get_transform_bias(ref_transforms)                 #Collecting the 0-frame bias
+    armature = find_armature()                                          #Finfing the Armature
+    set_armature_keyframes(armature, ref_transforms, transform_bias)    #Setting the Armature keyframes
+    for j in ref_transforms:
+        if j == "Bip01":
+            print(ref_transforms[j])
+            print("\n")
+    """for i in ref_transforms:
+        if i == 'Bip01':
+            print(transform_bias['Bip01'].values())"""
 
     bpy.ops.object.select_all(action='DESELECT')
+    #print(ref_transforms)
+    scene.frame_current = 0
     print("========Done========")
     
 
